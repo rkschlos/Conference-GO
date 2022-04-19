92a201e767f35a969bb8cf065ed179680f66d133
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-04-18 19:56:22 -0500

    split into microservice and monolith

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,19 +1,30 @@
 from django.http import JsonResponse
-from events.api_views import ConferenceListEncoder
-from .models import Attendee
-from events.models import Conference, Location
+from .models import Attendee, ConferenceVO
 from common.json import ModelEncoder
 from django.views.decorators.http import require_http_methods
 import json
 
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
 class AttendeeListEncoder(ModelEncoder):
     model = Attendee
-    properties = ["email", "name"]
+    properties = ["name"]
+
+
+class AttendeeDetailEncoder(ModelEncoder):
+    model = Attendee
+    properties = ["email", "name", "company_name", "created", "conference"]
+    encoders = {
+        "conference": ConferenceVODetailEncoder(),
+    }
 
 
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     """
     Lists the attendees names and the link to the attendee
     for the specified conference id.
@@ -34,21 +45,24 @@ def api_list_attendees(request, conference_id):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
         )
-    else:
+    else:  # "POST":
         content = json.loads(request.body)
+
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = content["conference"]
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
             )
+
         attendee = Attendee.objects.create(**content)
         return JsonResponse(
             attendee,
@@ -57,19 +71,7 @@ def api_list_attendees(request, conference_id):
         )
 
 
-class AttendeeDetailEncoder(ModelEncoder):
-    model = Attendee
-    properties = [
-        "email",
-        "name",
-        "company_name",
-        "created",
-    ]
-    encoders = {
-        "conference": ConferenceListEncoder(),
-    }
-
-
+# @require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_attendee(request, pk):
     """
     Returns the details for the Attendee model specified
@@ -90,9 +92,18 @@ def api_show_attendee(request, pk):
         }
     }
     """
+    # if request.method == "GET":
     attendee = Attendee.objects.get(id=pk)
     return JsonResponse(
         attendee,
         encoder=AttendeeDetailEncoder,
-        self=False,
+        safe=False,
     )
+    # elif request.method == "DELETE":
+    #   count, _ = Attendee.objects.filter(id=pk).delete()
+    #  return JsonResponse({"deleted": count > 0})
+    # else:
+    #     content = json.loads(request.body)
+    #     try:
+    #         if "conference" in content:
+    #             conference = Conference.objects.get(conference_id = id)
--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -6,59 +6,47 @@ import json
 from events.acl import find_pictures
 
 
+class LocationListEncoder(ModelEncoder):
+    model = Location
+    properties = ["name"]
+
+
+class LocationDetailEncoder(ModelEncoder):
+    model = Location
+    properties = [
+        "name",
+        "city",
+        "room_count",
+        "created",
+        "updated",
+        "picture_url",
+    ]
+
+    def get_extra_data(self, o):
+        return {"state": o.state.abbreviation}
+
+
 class ConferenceListEncoder(ModelEncoder):
     model = Conference
     properties = ["name"]
 
 
-@require_http_methods(["GET", "POST"])
-def api_list_conferences(request):
-    """
-    Lists the conference names and the link to the conference.
-
-    Returns a dictionary with a single key "conferences" which
-    is a list of conference names and URLS. Each entry in the list
-    is a dictionary that contains the name of the conference and
-    the link to the conference's information.
-
-    {
-        "conferences": [
-            {
-                "name": conference's name,
-                "href": URL to the conference,
-            },
-            ...
-        ]
+class ConferenceDetailEncoder(ModelEncoder):
+    model = Conference
+    properties = [
+        "name",
+        "description",
+        "max_presentations",
+        "max_attendees",
+        "starts",
+        "ends",
+        "created",
+        "updated",
+        "location",
+    ]
+    encoders = {
+        "location": LocationListEncoder(),
     }
-    """
-    if request.method == "GET":
-        conferences = Conference.objects.all()
-        return JsonResponse(
-            {"conferences": conferences},
-            encoder=ConferenceListEncoder,
-        )
-    else:
-        content = json.loads(request.body)
-        try:
-            location = Location.objects.get(id=content["location"])
-            content["location"] = location
-        except Location.DoesNotExist:
-            return JsonResponse(
-                {"message": "Invalid location id"},
-                status=400,
-            )
-
-        conference = Conference.objects.create(**content)
-        return JsonResponse(
-            conference,
-            encoder=ConferenceDetailEncoder,
-            safe=False,
-        )
-
-
-class LocationListEncoder(ModelEncoder):
-    model = Location
-    properties = ["name"]
 
 
 @require_http_methods(["GET", "POST"])
@@ -101,30 +89,12 @@ def api_list_locations(request):
 
         city = content["city"]
         photos = find_pictures(city)
-
-        print(city)
-
         content["picture_url"] = photos[0]["url"]
 
         location = Location.objects.create(**content)
         return JsonResponse(
             location, encoder=LocationDetailEncoder, safe=False
         )
-
-
-class LocationDetailEncoder(ModelEncoder):
-    model = Location
-    properties = [
-        "name",
-        "city",
-        "room_count",
-        "created",
-        "updated",
-        "picture_url",
-    ]
-
-    def get_extra_data(self, o):
-        return {"state": o.state.abbreviation}
 
 
 @require_http_methods(["DELETE", "GET", "PUT"])
@@ -175,22 +145,50 @@ def api_show_location(request, pk):
         )
 
 
-class ConferenceDetailEncoder(ModelEncoder):
-    model = Conference
-    properties = [
-        "name",
-        "description",
-        "max_presentations",
-        "max_attendees",
-        "starts",
-        "ends",
-        "created",
-        "updated",
-        "location",
-    ]
-    encoders = {
-        "location": LocationListEncoder(),
+@require_http_methods(["GET", "POST"])
+def api_list_conferences(request):
+    """
+    Lists the conference names and the link to the conference.
+
+    Returns a dictionary with a single key "conferences" which
+    is a list of conference names and URLS. Each entry in the list
+    is a dictionary that contains the name of the conference and
+    the link to the conference's information.
+
+    {
+        "conferences": [
+            {
+                "name": conference's name,
+                "href": URL to the conference,
+            },
+            ...
+        ]
     }
+    """
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences},
+            encoder=ConferenceListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference,
+            encoder=ConferenceDetailEncoder,
+            safe=False,
+        )
 
 
 def api_show_conference(request, pk):
